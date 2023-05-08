7672076b9855a0ca1bf371435ac6b43e5474221f
natha <natha@DESKTOP-7OSAR8O>
2023-05-08 11:27:19 -0700

    Add project files.

---
+++ Bagel House/Form1.Designer.vb
@@ -0,0 +1,421 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.radWhite = New System.Windows.Forms.RadioButton()
+        Me.radWheat = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.chkPeach = New System.Windows.Forms.CheckBox()
+        Me.chkRaspberry = New System.Windows.Forms.CheckBox()
+        Me.chkBlueberry = New System.Windows.Forms.CheckBox()
+        Me.chkButter = New System.Windows.Forms.CheckBox()
+        Me.chkCreamCheese = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.radDAFEAULAIT = New System.Windows.Forms.RadioButton()
+        Me.radCappuccino = New System.Windows.Forms.RadioButton()
+        Me.radRegCoffee = New System.Windows.Forms.RadioButton()
+        Me.radNoCoffee = New System.Windows.Forms.RadioButton()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.radStudent = New System.Windows.Forms.RadioButton()
+        Me.radSenior = New System.Windows.Forms.RadioButton()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.radNone = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'radWhite
+        '
+        Me.radWhite.AutoSize = True
+        Me.radWhite.Checked = True
+        Me.radWhite.Location = New System.Drawing.Point(6, 22)
+        Me.radWhite.Name = "radWhite"
+        Me.radWhite.Size = New System.Drawing.Size(94, 19)
+        Me.radWhite.TabIndex = 0
+        Me.radWhite.TabStop = True
+        Me.radWhite.Text = "Regular ($15)"
+        Me.radWhite.UseVisualStyleBackColor = True
+        '
+        'radWheat
+        '
+        Me.radWheat.AutoSize = True
+        Me.radWheat.Location = New System.Drawing.Point(6, 47)
+        Me.radWheat.Name = "radWheat"
+        Me.radWheat.Size = New System.Drawing.Size(78, 19)
+        Me.radWheat.TabIndex = 1
+        Me.radWheat.Text = "Fruit ($20)"
+        Me.radWheat.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radWhite)
+        Me.GroupBox1.Controls.Add(Me.radWheat)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 84)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pick a Head"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.chkPeach)
+        Me.GroupBox2.Controls.Add(Me.chkRaspberry)
+        Me.GroupBox2.Controls.Add(Me.chkBlueberry)
+        Me.GroupBox2.Controls.Add(Me.chkButter)
+        Me.GroupBox2.Controls.Add(Me.chkCreamCheese)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 191)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 147)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pick a Flavor"
+        '
+        'chkPeach
+        '
+        Me.chkPeach.AutoSize = True
+        Me.chkPeach.Location = New System.Drawing.Point(6, 122)
+        Me.chkPeach.Name = "chkPeach"
+        Me.chkPeach.Size = New System.Drawing.Size(118, 19)
+        Me.chkPeach.TabIndex = 4
+        Me.chkPeach.Text = "Cardamom ($.75)"
+        Me.chkPeach.UseVisualStyleBackColor = True
+        '
+        'chkRaspberry
+        '
+        Me.chkRaspberry.AutoSize = True
+        Me.chkRaspberry.Location = New System.Drawing.Point(6, 97)
+        Me.chkRaspberry.Name = "chkRaspberry"
+        Me.chkRaspberry.Size = New System.Drawing.Size(126, 19)
+        Me.chkRaspberry.TabIndex = 3
+        Me.chkRaspberry.Text = "Sweet Melon ($.75)"
+        Me.chkRaspberry.UseVisualStyleBackColor = True
+        '
+        'chkBlueberry
+        '
+        Me.chkBlueberry.AutoSize = True
+        Me.chkBlueberry.Location = New System.Drawing.Point(6, 72)
+        Me.chkBlueberry.Name = "chkBlueberry"
+        Me.chkBlueberry.Size = New System.Drawing.Size(83, 19)
+        Me.chkBlueberry.TabIndex = 2
+        Me.chkBlueberry.Text = "Mint ($.75)"
+        Me.chkBlueberry.UseVisualStyleBackColor = True
+        '
+        'chkButter
+        '
+        Me.chkButter.AutoSize = True
+        Me.chkButter.Location = New System.Drawing.Point(6, 47)
+        Me.chkButter.Name = "chkButter"
+        Me.chkButter.Size = New System.Drawing.Size(80, 19)
+        Me.chkButter.TabIndex = 1
+        Me.chkButter.Text = "Kiwi ($.25)"
+        Me.chkButter.UseVisualStyleBackColor = True
+        '
+        'chkCreamCheese
+        '
+        Me.chkCreamCheese.AutoSize = True
+        Me.chkCreamCheese.Location = New System.Drawing.Point(6, 22)
+        Me.chkCreamCheese.Name = "chkCreamCheese"
+        Me.chkCreamCheese.Size = New System.Drawing.Size(96, 19)
+        Me.chkCreamCheese.TabIndex = 0
+        Me.chkCreamCheese.Text = "Mango ($.50)"
+        Me.chkCreamCheese.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.radDAFEAULAIT)
+        Me.GroupBox3.Controls.Add(Me.radCappuccino)
+        Me.GroupBox3.Controls.Add(Me.radRegCoffee)
+        Me.GroupBox3.Controls.Add(Me.radNoCoffee)
+        Me.GroupBox3.Location = New System.Drawing.Point(230, 84)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(200, 125)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Want a Drink With That?"
+        '
+        'radDAFEAULAIT
+        '
+        Me.radDAFEAULAIT.AutoSize = True
+        Me.radDAFEAULAIT.Location = New System.Drawing.Point(6, 97)
+        Me.radDAFEAULAIT.Name = "radDAFEAULAIT"
+        Me.radDAFEAULAIT.Size = New System.Drawing.Size(122, 19)
+        Me.radDAFEAULAIT.TabIndex = 3
+        Me.radDAFEAULAIT.Text = "Cafe au lait ($1.75)"
+        Me.radDAFEAULAIT.UseVisualStyleBackColor = True
+        '
+        'radCappuccino
+        '
+        Me.radCappuccino.AutoSize = True
+        Me.radCappuccino.Location = New System.Drawing.Point(6, 72)
+        Me.radCappuccino.Name = "radCappuccino"
+        Me.radCappuccino.Size = New System.Drawing.Size(127, 19)
+        Me.radCappuccino.TabIndex = 2
+        Me.radCappuccino.Text = "Cappuccino ($2.00)"
+        Me.radCappuccino.UseVisualStyleBackColor = True
+        '
+        'radRegCoffee
+        '
+        Me.radRegCoffee.AutoSize = True
+        Me.radRegCoffee.Checked = True
+        Me.radRegCoffee.Location = New System.Drawing.Point(6, 47)
+        Me.radRegCoffee.Name = "radRegCoffee"
+        Me.radRegCoffee.Size = New System.Drawing.Size(141, 19)
+        Me.radRegCoffee.TabIndex = 1
+        Me.radRegCoffee.TabStop = True
+        Me.radRegCoffee.Text = "Regular Coffee ($1.25)"
+        Me.radRegCoffee.UseVisualStyleBackColor = True
+        '
+        'radNoCoffee
+        '
+        Me.radNoCoffee.AutoSize = True
+        Me.radNoCoffee.Location = New System.Drawing.Point(6, 22)
+        Me.radNoCoffee.Name = "radNoCoffee"
+        Me.radNoCoffee.Size = New System.Drawing.Size(54, 19)
+        Me.radNoCoffee.TabIndex = 0
+        Me.radNoCoffee.Text = "None"
+        Me.radNoCoffee.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.lblSubtotal)
+        Me.GroupBox4.Controls.Add(Me.lblTotal)
+        Me.GroupBox4.Controls.Add(Me.lblTax)
+        Me.GroupBox4.Controls.Add(Me.Label4)
+        Me.GroupBox4.Controls.Add(Me.Label2)
+        Me.GroupBox4.Controls.Add(Me.Label3)
+        Me.GroupBox4.Location = New System.Drawing.Point(230, 238)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox4.TabIndex = 0
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Price"
+        '
+        'lblSubtotal
+        '
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSubtotal.Location = New System.Drawing.Point(94, 11)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblSubtotal.TabIndex = 9
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(94, 73)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal.TabIndex = 8
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Location = New System.Drawing.Point(94, 44)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(100, 23)
+        Me.lblTax.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 81)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Total"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Subtotal"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(64, 52)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Tax"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(98, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(281, 28)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Rose Cafe Hookah Lounge"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 353)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "&Calculate Total"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(182, 353)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "&Reset Form"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(355, 353)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "E&xit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'radStudent
+        '
+        Me.radStudent.AutoSize = True
+        Me.radStudent.Location = New System.Drawing.Point(6, 38)
+        Me.radStudent.Name = "radStudent"
+        Me.radStudent.Size = New System.Drawing.Size(104, 19)
+        Me.radStudent.TabIndex = 11
+        Me.radStudent.Text = "Student (-10%)"
+        Me.radStudent.UseVisualStyleBackColor = True
+        '
+        'radSenior
+        '
+        Me.radSenior.AutoSize = True
+        Me.radSenior.Location = New System.Drawing.Point(6, 63)
+        Me.radSenior.Name = "radSenior"
+        Me.radSenior.Size = New System.Drawing.Size(135, 19)
+        Me.radSenior.TabIndex = 12
+        Me.radSenior.Text = "Senior Citizen (-20%)"
+        Me.radSenior.UseVisualStyleBackColor = True
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.radNone)
+        Me.GroupBox5.Controls.Add(Me.radStudent)
+        Me.GroupBox5.Controls.Add(Me.radSenior)
+        Me.GroupBox5.Location = New System.Drawing.Point(436, 84)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(200, 125)
+        Me.GroupBox5.TabIndex = 13
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Discounts"
+        '
+        'radNone
+        '
+        Me.radNone.AutoSize = True
+        Me.radNone.Checked = True
+        Me.radNone.Location = New System.Drawing.Point(6, 13)
+        Me.radNone.Name = "radNone"
+        Me.radNone.Size = New System.Drawing.Size(54, 19)
+        Me.radNone.TabIndex = 13
+        Me.radNone.TabStop = True
+        Me.radNone.Text = "None"
+        Me.radNone.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Bagel and Coffee Price Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents radWhite As RadioButton
+    Friend WithEvents radWheat As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents chkCreamCheese As CheckBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents chkButter As CheckBox
+    Friend WithEvents chkBlueberry As CheckBox
+    Friend WithEvents chkRaspberry As CheckBox
+    Friend WithEvents chkPeach As CheckBox
+    Friend WithEvents radDAFEAULAIT As RadioButton
+    Friend WithEvents radCappuccino As RadioButton
+    Friend WithEvents radRegCoffee As RadioButton
+    Friend WithEvents radNoCoffee As RadioButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents radStudent As RadioButton
+    Friend WithEvents radSenior As RadioButton
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents radNone As RadioButton
+End Class
---
+++ Bagel House/Form1.vb
@@ -0,0 +1,135 @@
+﻿Public Class Form1
+    ' Class-level declarations
+    Const decTAX_RATE As Decimal = 0.06D ' Tax rate
+    Const decWHITE_BAGEL As Decimal = 15D ' Cost of regular Hookah
+    Const decWHEAT_BAGEL As Decimal = 20D ' Cost of a fruit Hookah
+    Const decCREAM_CHEESE As Decimal = 0.5D ' Cost of cream cheese topping
+    Const decBUTTER As Decimal = 0.25D ' Cost of butter topping
+    Const decBLUEBERRY As Decimal = 0.75D ' Cost of blueberry topping
+    Const decRASPBERRY As Decimal = 0.75D ' Cost of raspberry topping
+    Const decPEACH As Decimal = 0.75D ' Cost of peach topping
+    Const decREG_COFFEE As Decimal = 1.25D ' Cost of regular coffee
+    Const decCAPPUCCINO As Decimal = 2D ' Cost of cappuccino
+    Const decCAFE_AU_LAIT As Decimal = 1.75D ' Cost of Cafe au lait
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles chkButter.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles radDAFEAULAIT.CheckedChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' This procedure calculates the total of an order.
+        Dim decSubtotal As Decimal ' Holds the order subtotal
+        Dim decTax As Decimal ' Holds the sales tax
+        Dim decTotal As Decimal ' Holds the order total
+        decSubtotal = CalcBagelCost() + CalcToppingCost() + CalcCoffeeCost()
+        decTax = CalcTax(decSubtotal)
+        decTotal = decSubtotal + decTax
+        lblSubtotal.Text = decSubtotal.ToString("c")
+        lblTax.Text = decTax.ToString("c")
+        lblTotal.Text = decTotal.ToString("c")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' This procedure resets the controls to default values.
+        ResetBagels()
+        ResetToppings()
+        ResetCoffee()
+        ResetPrice()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+    Function CalcBagelCost() As Decimal
+        ' This function returns the cost of the selected bagel.
+        Dim decBagel As Decimal
+        If radWhite.Checked = True Then
+            decBagel = decWHITE_BAGEL
+        Else
+            decBagel = decWHEAT_BAGEL
+        End If
+        Return decBagel
+    End Function
+    Function CalcToppingCost() As Decimal
+        ' This function returns the cost of the toppings.
+        Dim decCostOfTopping As Decimal = 0D
+        If chkCreamCheese.Checked = True Then
+            decCostOfTopping += decCREAM_CHEESE
+        End If
+        If chkButter.Checked = True Then
+            decCostOfTopping += decBUTTER
+        End If
+        If chkBlueberry.Checked = True Then
+            decCostOfTopping += decBLUEBERRY
+        End If
+        If chkRaspberry.Checked = True Then
+            decCostOfTopping += decRASPBERRY
+        End If
+        If chkPeach.Checked = True Then
+            decCostOfTopping += decPEACH
+        End If
+        Return decCostOfTopping
+    End Function
+    Function CalcCoffeeCost() As Decimal
+        ' This function returns the cost of the selected coffee.
+        Dim decCoffee As Decimal
+        If radNoCoffee.Checked Then
+            decCoffee = 0D
+        ElseIf radRegCoffee.Checked = True Then
+            decCoffee = decREG_COFFEE
+        ElseIf radCappuccino.Checked = True Then
+            decCoffee = decCAPPUCCINO
+        ElseIf radDAFEAULAIT.Checked = True Then
+            decCoffee = decCAFE_AU_LAIT
+        End If
+        Return decCoffee
+
+    End Function
+    Function CalcTax(ByVal decAmount As Decimal) As Decimal
+        ' This function receives the sale amount and
+        ' returns the amount of sales tax.
+        Return decAmount * decTAX_RATE
+    End Function
+    Sub ResetBagels()
+        ' This procedure resets the bagel selection.
+        radWhite.Checked = True
+    End Sub
+    Sub ResetToppings()
+        ' This procedure resets the topping selection.
+        chkCreamCheese.Checked = False
+        chkButter.Checked = False
+        chkBlueberry.Checked = False
+        chkRaspberry.Checked = False
+        chkPeach.Checked = False
+    End Sub
+    Sub ResetCoffee()
+        ' This procedure resets the coffee selection.
+        radRegCoffee.Checked = True
+    End Sub
+    Sub ResetPrice()
+        ' This procedure resets the price.
+        lblSubtotal.Text = String.Empty
+        lblTax.Text = String.Empty
+        lblTotal.Text = String.Empty
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles radStudent.CheckedChanged
+
+    End Sub
+
+    Private Sub GroupBox5_Enter(sender As Object, e As EventArgs) Handles GroupBox5.Enter
+
+    End Sub
+End Class
